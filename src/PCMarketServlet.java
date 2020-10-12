5da496e12bf58f9bdf02c933baf836841d79e775
rrmarquis <72050027+rrmarquis@users.noreply.github.com>
2020-10-12 12:40:43 -0500

    Update PCMarketServlet.java
    Servlet update with imports and lists for Motherboard, CPU, and Hard_Drive.

--- src/PCMarketServlet.java
+++ src/PCMarketServlet.java
@@ -1,41 +1,65 @@
-
-
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PCMarketServlet
- */
+import datamodel.Motherboard;
+import datamodel.CPU;
+import datamodel.Hard_Drive;
+import util.UtilDBPCMarket;
+
 @WebServlet("/PCMarketServlet")
 public class PCMarketServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PCMarketServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+   private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("PC Market");
-	}
+   public PCMarketServlet() {
+      super();
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      response.setContentType("text/html");
+      retrieveDisplayData(response.getWriter());
+   }
 
+   void retrieveDisplayData(PrintWriter out) {
+      String title = "Database Result";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
+            "transitional//en\">\n"; //
+      out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor=\"#f0f0f0\">\n" + //
+            "<h1 align=\"center\">" + title + "</h1>\n");
+      out.println("<ul>");
+      List<Motherboard> listMobos = UtilDBPCMarket.listMotherboard();
+      for (Motherboard mobos : listMobos) {
+          System.out.println("<li>" + mobos.getId() + ", " //
+               + mobos.getSocket() + ", " //
+               + mobos.getForm() + "</li>");
+      }
+      List<CPU> listCPUs = UtilDBPCMarket.listCPU();
+      for (CPU cpus : listCPUs) {
+          System.out.println("<li>" + cpus.getId() + ", " //
+               + cpus.getNumOfCores() + ", " //
+               + cpus.getCacheSize() + ", " //
+               + cpus.getSocketCompatibility() + "</li>");
+      }
+      List<Hard_Drive> listDrives = UtilDBPCMarket.listHardDrive();
+      for (Hard_Drive drives : listDrives) {
+          System.out.println("<li>" + drives.getId() + ", " //
+               + drives.getStorageSpace() + ", " //
+               + drives.getRotationsPerMinute() + "</li>");
+      }
+      out.println("</ul>");
+      out.println("</body></html>");
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
 }
