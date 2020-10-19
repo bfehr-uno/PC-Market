279349ddd40426d317985fa4fe863dfc1a73677b
mlush-27 <62492176+mlush-27@users.noreply.github.com>
2020-10-19 14:34:59 -0500

    Add files via upload
    Updated MySQL File

--- WebContent/META-INF/PCMarketDB_MySQL.sql
+++ WebContent/META-INF/PCMarketDB_MySQL.sql
@@ -1,12 +1,14 @@
 CREATE TABLE Part_Type (id INT NOT NULL AUTO_INCREMENT,
 						Motherboard_id INT,
 						CPU_id INT,
+						GPU_id INT,
 						Hard_Drive_id INT,
 						User VARCHAR(75) NOT NULL,
 						Price VARCHAR(75) NOT NULL,
-						FOREIGN KEY (Motherboard_id) REFERENCES Motherboard_Part(id),
-						FOREIGN KEY (CPU_id) REFERENCES CPU_Part(id),
-						FOREIGN KEY (Hard_Drive_id) REFERENCES Hard_Drive_Part(id),
+						FOREIGN KEY (Motherboard_id) REFERENCES Motherboard(id),
+						FOREIGN KEY (CPU_id) REFERENCES CPU(id),
+						FOREIGN KEY (GPU_id) REFERENCES GPU(id),
+						FOREIGN KEY (Hard_Drive_id) REFERENCES Hard_Drive(id),
 						PRIMARY KEY (ID));
 
 CREATE TABLE Motherboard (id INT NOT NULL AUTO_INCREMENT,
@@ -29,7 +31,7 @@ CREATE TABLE GPU (id INT NOT NULL AUTO_INCREMENT,
 				  Manufacturer VARCHAR(75) NOT NULL,
 				  Model_Name VARCHAR(75) NOT NULL,
 				  Port_Type VARCHAR(75) NOT NULL,
-				  Interface VARCHAR(75) NOT NULL,
+				  Interface_Type_Type VARCHAR(75) NOT NULL,
 				  Memory_Size VARCHAR(75) NOT NULL,
 				  PRIMARY KEY (ID));
 
@@ -44,7 +46,7 @@ CREATE TABLE Hard_Drive (id INT NOT NULL AUTO_INCREMENT,
 /*INSERT STATMENTS*/
 INSERT INTO Motherboard (id, Manufacturer, Model_Name, Socket, Form_Factor) values (default, ?, ?, ?);
 INSERT INTO CPU (id, Manufacturer, Model_Name, Num_of_Cores, Frequency, Socket_Compatibility) values (default, ?, ?, ?);
-INSERT INTO GPU (id, Manufacturer, Model_Name, Port_Type, Interface, Memory_Size) values (default, ?, ?, ?, ?, ?);
+INSERT INTO GPU (id, Manufacturer, Model_Name, Port_Type, Interface_Type, Memory_Size) values (default, ?, ?, ?, ?, ?);
 INSERT INTO Hard_Drive (id, Manufacturer, Model_Name, Storage_Space, Rotations_Per_Minute) values (default, ?, ?, ?);
 
 /*REMOVE STATMENTS*/
@@ -57,12 +59,12 @@ DELETE FROM Hard_Drive WHERE id = ?;
 /*SELECT ALL*/
 SELECT M.Manufacturer, M.Model_Name, M.Socket, M.Form_Factor, 
 	   C.Manufacturer, C.Model_Name, C.Num_of_Cores, C.Frequency, C.Socket_Compatibility,
-	   G.Manufacturer, G.Model_Name, G.Port_Type, G.Interface, G.Memory_Size,
+	   G.Manufacturer, G.Model_Name, G.Port_Type, G.Interface_Type, G.Memory_Size,
 	   H.Manufacturer, H.Model_Name, H.Storage_Space, H.Rotations_Per_Minute,
 FROM Motherboard M, CPU C, GPU G, Hard_Drive H
 ORDER BY M.Manufacturer, M.Model_Name, M.Socket, M.Form_Factor,
 		 C.Manufacturer, C.Model_Name, C.Num_of_Cores, C.Frequency,  C.Socket_Compatibility,
-		 G.Manufacturer, G.Model_Name, G.Port_Type, G.Interface, G.Memory_Size,
+		 G.Manufacturer, G.Model_Name, G.Port_Type, G.Interface_Type, G.Memory_Size,
 		 H.Manufacturer, H.Model_Name, H.Storage_Space, H.Rotations_Per_Minute,
 		 M.id, C.id, H.id;
 
