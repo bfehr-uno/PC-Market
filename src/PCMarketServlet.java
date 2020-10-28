f124cd8c4ef1c144107e1019f3de9de26939c1de
mlush-27 <62492176+mlush-27@users.noreply.github.com>
2020-10-27 21:07:22 -0500

    Add files via upload
    PCMarketServlet.java now displays [DBG] output to system and entire database to webpage.

--- src/PCMarketServlet.java
+++ src/PCMarketServlet.java
@@ -23,79 +23,132 @@ public class PCMarketServlet extends HttpServlet {
    }
 
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      response.setContentType("text/html");
-      
-      UtilDBPCMarket.createCPUs("Intel", "Core i7-9700K", "8-Core", "4.9 GHz", "LGA 1151 (300 Series)", "$250");
-      UtilDBPCMarket.createCPUs("AMD", "Ryzen 7 3800XT", "8-Core", "4.7 GHz", "Socket AM4", "$350");
-      UtilDBPCMarket.createCPUs("Intel", "Core i9-9900K", "8-Core", "5.0 GHz", "LGA 1151 (300 Series)", "$350");
-		
-      UtilDBPCMarket.createGPUs("MSI", "RTX 2080 Super Gaming X TRIO", "1 x HDMI 2.0b 3 x DisplayPort 1.4", "PCI Express 3.0 x16", "8GB", "$1000");
-      UtilDBPCMarket.createGPUs("GIGABYTE", "GV-N2060OC-6GD ver 2.0", "1 x HDMI 2.0b 3 x DisplayPort 1.4", "PCI Express 3.0 x16", "6GB", "$280");
-      UtilDBPCMarket.createGPUs("MSI", "RX 5700 XT GAMING X", "1 x HDMI 2.0b 3 x DisplayPort 1.4", "PCI Express 4.0", "8GB", "$350");
-		
-      UtilDBPCMarket.createHard_Drives("Samsung", "ST2000DL004", "2TB", "5400 RPM", "$250");
-      UtilDBPCMarket.createHard_Drives("Samsung", "HD154UI", "1.5TB", "7200 RPM", "$60");
-      UtilDBPCMarket.createHard_Drives("Water Panther", "WPC101200GSAS-G7", "1.2TB", "10000 RPM", "$75");
-		
-      UtilDBPCMarket.createMotherboards("MSI", "MPG Z490 GAMING EDGE WIFI", "LGA 1200", "2 x PCI Express 3.0 x16", "ATX", "$180");
-      UtilDBPCMarket.createMotherboards("ASUS", "STRIX Z490-E GAMING", "LGA 1200", "2 x PCIe 3.0 x16 (x16 or dual x8)", "ATX", "$225");
-      UtilDBPCMarket.createMotherboards("GIGABYTE", "Z390 AORUS PRO WIFI", "LGA 1151 (300 Series)", "1 x PCI Express x16 slot, running at x16 (PCIEX16)", "ATX", "$125");
-      
-      retrieveDisplayData(response.getWriter());
+	   response.setContentType("text/html");
+	   
+	   UtilDBPCMarket.createCPUs("Intel", "Core i7-9700K", "8-Core", 
+			   					 "4.9 GHz", "LGA 1151 (300 Series)", "$250");
+	   UtilDBPCMarket.createCPUs("AMD", "Ryzen 7 3800XT", "8-Core", 
+								 "4.7 GHz", "Socket AM4", "$350");
+	   UtilDBPCMarket.createCPUs("Intel", "Core i9-9900K", "8-Core", 
+								 "5.0 GHz", "LGA 1151 (300 Series)", "$350");
+	   
+	   UtilDBPCMarket.createGPUs("MSI", "RTX 2080 Super Gaming X TRIO", 
+								 "1 x HDMI 2.0b 3 x DisplayPort 1.4", 
+								 "PCI Express 3.0 x16", "8GB", "$1000");
+	   UtilDBPCMarket.createGPUs("GIGABYTE", "GV-N2060OC-6GD ver 2.0", 
+								 "1 x HDMI 2.0b 3 x DisplayPort 1.4", 
+								 "PCI Express 3.0 x16", "6GB", "$280");
+	   UtilDBPCMarket.createGPUs("MSI", "RX 5700 XT GAMING X", 
+								 "1 x HDMI 2.0b 3 x DisplayPort 1.4", 
+								 "PCI Express 4.0", "8GB", "$350");
+	   
+	   UtilDBPCMarket.createHard_Drives("Samsung", "ST2000DL004", "2TB", 
+										"5400 RPM", "$250");
+	   UtilDBPCMarket.createHard_Drives("Samsung", "HD154UI", "1.5TB", 
+										"7200 RPM", "$60");
+	   UtilDBPCMarket.createHard_Drives("Water Panther", "WPC101200GSAS-G7", "1.2TB", 
+										"10000 RPM", "$75");
+	   
+	   UtilDBPCMarket.createMotherboards("MSI", "MPG Z490 GAMING EDGE WIFI", 
+										 "LGA 1200", "2 x PCI Express 3.0 x16", 
+										 "ATX", "$180");
+	   UtilDBPCMarket.createMotherboards("ASUS", "STRIX Z490-E GAMING", 
+										 "LGA 1200", "2 x PCIe 3.0 x16 (x16 or dual x8)", 
+										 "ATX", "$225");
+	   UtilDBPCMarket.createMotherboards("GIGABYTE", "Z390 AORUS PRO WIFI", 
+			   							 "LGA 1151 (300 Series)", 
+										 "1 x PCI Express x16 slot, running at x16 (PCIEX16)", 
+										 "ATX", "$125");
+	   
+	   retrieveDisplayData(response.getWriter());
    }
 
    void retrieveDisplayData(PrintWriter out) {
-      String title = "Database Result";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
-            "transitional//en\">\n"; //
-      out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h1 align=\"center\">" + title + "</h1>\n");
-      out.println("<ul>");
-      
-	      List<Motherboard> listMobos = UtilDBPCMarket.listMotherboard();
-	      for (Motherboard mobos : listMobos) {
-	          System.out.println("<li>" + mobos.getId() + ", " //
-	        		  + mobos.getManufacturer() + ", " //
-	        		  + mobos.getModelName() + ", " //
-	        		  + mobos.getSocket() + ", " //
-	        		  + mobos.getExpansionSlots() + ", " //
-	        		  + mobos.getFormFactor() + ", " //
-	        		  + mobos.getPrice() + "</li>");
-	      }
-	      List<CPU> listCPUs = UtilDBPCMarket.listCPU();
-	      for (CPU cpus : listCPUs) {
-	          System.out.println("<li>" + cpus.getId() + ", " //
-	        		  + cpus.getManufacturer() + ", " //
-	        		  + cpus.getModelName() + ", " //
-	        		  + cpus.getNumOfCores() + ", " //
-	        		  + cpus.getFreq() + ", " //
-	        		  + cpus.getSocketCompatibility() + ", " //
-	        		  + cpus.getPrice() + "</li>");
-	      }
-	      List<GPU> listGPUs = UtilDBPCMarket.listGPU();
-	      for (GPU gpus : listGPUs) {
-	          System.out.println("<li>" + gpus.getId() + ", " //
-	        		  + gpus.getManufacturer() + ", " //
-	        		  + gpus.getModelName() + ", " //
-	        		  + gpus.getPortType() + ", " //
-	        		  + gpus.getInterfaceType() + ", " //
-	        		  + gpus.getMemorySize() + ", " //
-	        		  + gpus.getPrice() + "</li>");
-	      }
-	      List<Hard_Drive> listDrives = UtilDBPCMarket.listHardDrive();
-	      for (Hard_Drive drives : listDrives) {
-	          System.out.println("<li>" + drives.getId() + ", " //
-	        		  + drives.getManufacturer() + ", " //
-	        		  + drives.getModelName() + ", " //
-	        		  + drives.getStorageSpace() + ", " //
-	        		  + drives.getRotationsPerMinute() + ", " //
-	        		  + drives.getPrice() + "</li>");
-	      }
-      out.println("</ul>");
-      out.println("</body></html>");
+	   String title = "Database Result";
+	   String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + //
+			   			"transitional//en\">\n"; //
+	   out.println(docType + //
+			   	   "<html>\n" + //
+			   	   "<head><title>" + title + "</title></head>\n" + //
+			   	   "<body bgcolor=\"#f0f0f0\">\n" + //
+			   	   "<h1 align=\"center\">" + title + "</h1>\n");
+	   out.println("<ul>");
+	   
+	   List<Motherboard> listMobos = UtilDBPCMarket.listMotherboard();
+	   for (Motherboard mobos : listMobos) {
+		   System.out.println("[DBG] " + mobos.getId() + ", " //
+				   			  + mobos.getManufacturer() + ", " //
+				   			  + mobos.getModelName() + ", " //
+				   			  + mobos.getSocket() + ", " //
+				   			  + mobos.getExpansionSlots() + ", " //
+				   			  + mobos.getFormFactor() + ", " //
+				   			  + mobos.getPrice());
+		   
+		   out.println("<li>" + mobos.getId() + ", " //
+				   	   + mobos.getManufacturer() + ", " //
+		   			   + mobos.getModelName() + ", " //
+		   			   + mobos.getSocket() + ", " //
+		   			   + mobos.getExpansionSlots() + ", " //
+		   			   + mobos.getFormFactor() + ", " //
+		   			   + mobos.getPrice() + "</li>");
+	   }
+	   
+	   List<CPU> listCPUs = UtilDBPCMarket.listCPU();
+	   for (CPU cpus : listCPUs) {
+		   System.out.println("[DBG] " + cpus.getId() + ", " //
+				   			  + cpus.getManufacturer() + ", " //
+				   			  + cpus.getModelName() + ", " //
+				   			  + cpus.getNumOfCores() + ", " //
+				   			  + cpus.getFreq() + ", " //
+				   			  + cpus.getSocketCompatibility() + ", " //
+				   			  + cpus.getPrice());
+		   
+		   out.println("<li>" + cpus.getId() + ", " //
+		   			   + cpus.getManufacturer() + ", " //
+		   			   + cpus.getModelName() + ", " //
+		   			   + cpus.getNumOfCores() + ", " //
+		   			   + cpus.getFreq() + ", " //
+		   			   + cpus.getSocketCompatibility() + ", " //
+		   			   + cpus.getPrice() + "</li>");
+	   }
+	   
+	   List<GPU> listGPUs = UtilDBPCMarket.listGPU();
+	   for (GPU gpus : listGPUs) {
+		   System.out.println("[DBG] " + gpus.getId() + ", " //
+				   			  + gpus.getManufacturer() + ", " //
+				   			  + gpus.getModelName() + ", " //
+				   			  + gpus.getPortType() + ", " //
+				   			  + gpus.getInterfaceType() + ", " //
+				   			  + gpus.getMemorySize() + ", " //
+				   			  + gpus.getPrice());
+		   
+		   out.println("<li>" + gpus.getId() + ", " //
+		   			   + gpus.getManufacturer() + ", " //
+		   			   + gpus.getModelName() + ", " //
+		   			   + gpus.getPortType() + ", " //
+		   			   + gpus.getInterfaceType() + ", " //
+		   			   + gpus.getMemorySize() + ", " //
+		   			   + gpus.getPrice() + "</li>");
+	   }
+	   
+	   List<Hard_Drive> listDrives = UtilDBPCMarket.listHardDrive();
+	   for (Hard_Drive drives : listDrives) {
+		   System.out.println("[DBG] " + drives.getId() + ", " //
+				   			  + drives.getManufacturer() + ", " //
+				   			  + drives.getModelName() + ", " //
+				   			  + drives.getStorageSpace() + ", " //
+				   			  + drives.getRotationsPerMinute() + ", " //
+				   			  + drives.getPrice());
+		   
+		   out.println("<li>" + drives.getId() + ", " //
+		   			   + drives.getManufacturer() + ", " //
+		   			   + drives.getModelName() + ", " //
+		   			   + drives.getStorageSpace() + ", " //
+		   			   + drives.getRotationsPerMinute() + ", " //
+		   			   + drives.getPrice() + "</li>");
+	   }
+	   out.println("</ul>");
+	   out.println("</body></html>");
    }
 
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
